bfcf1a18491ba8eb45432a1002a2f862145d4c17
liero <turand@centrum.sk>
2021-06-01 15:27:22 +0200

    Added cancellation token to ReadEventArgs

--- BlazorComponents/Grid/ReadEventArgs.cs
+++ BlazorComponents/Grid/ReadEventArgs.cs
@@ -1,20 +1,23 @@
 ﻿#pragma warning disable CS8714 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'notnull' constraint.
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace vNext.BlazorComponents.Grid
 {
     public class ReadEventArgs<TRow>
     {
 
-        public ReadEventArgs(int startIndex, int count)
+        public ReadEventArgs(int startIndex, int count, CancellationToken cancellationToken = default(CancellationToken))
         {
             StartIndex = startIndex;
             Count = count;
+            CancellationToken = cancellationToken;
         }
 
         public int StartIndex { get; }
         public int Count { get; }
+        public CancellationToken CancellationToken { get; }
         public int? Total { get; set; }
         public IEnumerable<TRow> Items { get; set; } = Enumerable.Empty<TRow>();
     }
