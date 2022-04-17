486975b3130573ed9e6c5a32724dce123216da2b
liero <turand@centrum.sk>
2022-04-17 14:16:15 +0200

    Added cell navigation using javascript. Added delete key support

---
+++ BlazorComponents/Grid/RowDeleteEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace vNext.BlazorComponents.Grid
+{
+    public class RowDeleteEventArgs<TRow>
+    {
+        public RowDeleteEventArgs(Row<TRow> row)
+        {
+            Row = row;
+        }
+        public TRow Data => Row.Data!;
+        public Row<TRow> Row { get; }
+    }
+}
---
+++ BlazorComponents/Grid/RowKeyboardEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace vNext.BlazorComponents.Grid
+{
+    public class RowKeyboardEventArgs<TRow>
+    {
+        public RowKeyboardEventArgs(Row<TRow> row, KeyboardEventArgs eventArgs)
+        {
+            Row = row;
+            EventArgs = eventArgs;
+        }
+
+        public TRow Data => Row.Data!;
+        public int? Index => Row?.Index;
+        public Row<TRow> Row { get; }
+        public KeyboardEventArgs EventArgs { get; }
+    }
+}
